887d63dafef0a0d06e0763bff5745752e09db340
nicoryo <nicoryo.giogio@gmail.com>
2020-09-10 18:31:39 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_10_071727) do
+ActiveRecord::Schema.define(version: 2020_09_10_092841) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "バリデーション" do
-    it "nameの値が空の場合、エラーを返すこと" do
-      user = User.new(name: "", email: "test@example.com")
-      expect(user.valid?).to eq(true)
-    end
-
-    it "emailの値が空の場合、エラーを返すこと" do
-      user = User.new(name: "TestUser", email: "")
-      expect(user.valid?).to eq(true)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
